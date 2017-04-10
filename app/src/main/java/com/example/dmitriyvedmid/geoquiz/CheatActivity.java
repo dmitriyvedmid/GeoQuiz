9b41571d35065cb6b4d30b90b19d6bee4a352c23
Dmitriy Vedmid <dmitriyvedmid@gmail.com>
2017-04-10 17:08:33 +0300

    cleared

--- app/src/main/java/com/example/dmitriyvedmid/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/dmitriyvedmid/geoquiz/CheatActivity.java
@@ -22,8 +22,6 @@ public class CheatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
 
-        System.out.println("hello");
-
         answerTV=(TextView)findViewById(R.id.answer_textview);
         showAnswerBtn=(Button)findViewById(R.id.show_answer_btn);
 
